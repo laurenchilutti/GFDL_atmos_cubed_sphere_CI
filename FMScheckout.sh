756a15fd9db1b105447cf7cea30cefe5039f3cec
Lauren Chilutti <lauren.chilutti@noaa.gov>
2023-09-27 16:10:41 +0000

    scripts for FMSregression

--- FMScheckout.sh
+++ FMScheckout.sh
@@ -20,13 +20,20 @@ if [ -z "$1" ]
     echo FMS tag is ${1}
     FMStag=${1}
 fi
+if [ ! -z "$2" ]
+  then
+    echo FMSCoupler tag is ${2}
+    FMSCtag=${2}
+fi
+if [ ! -z "$3" ]
+  then
+    echo atmos_drivers tag is ${3}
+    ADtag=${3}
 
 testDir=${dirRoot}/${intelVersion}/${FMStag}
-logDir=${testDir}/log
-export MODULESHOME=/usr/share/lmod/lmod
 ## create directories
 rm -rf ${testDir}
-mkdir -p ${logDir}
+mkdir -p ${testDir}
 
 ## clone code
 cd ${testDir}
@@ -35,3 +42,7 @@ git clone --recursive https://github.com/NOAA-GFDL/SHiELD_build.git
 ## Check out the FMS tag
 mkdir -p ${testDir}/SHiELD_SRC
 cd ${testDir}/SHiELD_SRC/ && git clone -b ${FMStag} https://github.com/NOAA-GFDL/FMS.git
+cd ${testDir}/SHiELD_SRC/ && git clone -b ${FMSCtag} https://github.com/NOAA-GFDL/FMSCoupler
+cd ${testDir}/SHiELD_SRC/ && git clone -b ${ADtag} https://github.com/NOAA-GFDL/atmos_drivers
+cd ${testDir}/SHiELD_SRC/ && git clone -b main https://github.com/NOAA-GFDL/GFDL_atmos_cubed_sphere
+cd ${testDir}/SHiELD_SRC/ && git clone -b main https://github.com/NOAA-GFDL/SHiELD_physics
---
+++ FMSswcompile.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -xe
+
+##############################################################################
+## User set up variables
+## Root directory for CI
+dirRoot=/contrib/fv3
+## Intel version to be used
+intelVersion=2023.2.0
+##############################################################################
+## HPC-ME container
+container=/contrib/containers/noaa-intel-prototype_2023.09.25.sif
+container_env_script=/contrib/containers/load_spack_noaa-intel.sh
+##############################################################################
+## Set up the directories
+if [ -z "$1" ]
+  then
+    echo "Error: No FMS tag supplied, please rerun and provide a tag to use for FMS"
+    exit 1
+  else
+    echo FMS tag is ${1}
+    FMStag=${1}
+fi
+testDir=${dirRoot}/${intelVersion}/${FMStag}
+
+# Set up build
+cd ${testDir}/SHiELD_build/Build
+#Define External Libs path
+export EXTERNAL_LIBS=${dirRoot}/FMSexternallibs
+
+# Build SHiELD
+set -o pipefail
+singularity exec -B /contrib ${container} ${container_env_script} "./COMPILE solo sw 64bit repro intel clean"
+
